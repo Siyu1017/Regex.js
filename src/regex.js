44524c4e5d60b69c9e4501bb17b1336c17cb4c03
Wet Rain <98018258+Siyu1017@users.noreply.github.com>
2023-07-21 17:58:20 +0800

    Update

---
+++ src/regex.js
@@ -0,0 +1,18 @@
+/**
+ * All rights reserved.
+ * Copyright (c) 2023 
+ * Author WetRain
+ */
+
+"use strict";
+
+class Regex {
+    list = {
+        color: {
+            "hex3": ""
+        }
+    }
+    constructor (name) {
+
+    }
+}
